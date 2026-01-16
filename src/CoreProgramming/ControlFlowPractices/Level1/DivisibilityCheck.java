083ace7aa15fec378a7c25e5d442d561a24bc6b1
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 15:26:38 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level1/DivisibilityCheck.java
+++ src/CoreProgramming/ControlFlowPractices/Level1/DivisibilityCheck.java
@@ -1,11 +1,12 @@
 package CoreProgramming.ControlFlowPractices.Level1;
-
 import java.util.Scanner;
 public class DivisibilityCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Getting user input
         System.out.print("Enter a number: ");
         int number = input.nextInt();
+        // Setting loop to check for divisibility
         if ( number % 5 == 0){
             System.out.println("Is the number " + number + " divisible by 5? Yes");
         }
