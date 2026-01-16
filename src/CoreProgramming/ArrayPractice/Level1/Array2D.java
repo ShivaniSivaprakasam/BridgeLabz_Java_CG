003deb6fd6e4c22be57ab693a40e504d7c919980
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 11:44:50 +0530

    Update Array2D.java
    Added comment lines to the code

--- src/CoreProgramming/ArrayPractice/Level1/Array2D.java
+++ src/CoreProgramming/ArrayPractice/Level1/Array2D.java
@@ -2,19 +2,26 @@ package CoreProgramming.ArrayPractice.Level1;
 import java.util.Scanner;
 public class Array2D {
     public static void main(String[] args) {
+        // Getting input from the user
         Scanner input = new Scanner(System.in);
         System.out.print("Enter rows: ");
+        // Input for number of rows
         int rows = input.nextInt();
         System.out.print("Enter columns: ");
+        // Input for number of columns
         int columns = input.nextInt();
+        // Initialization of a 2D array
         int[][] arr = new int[rows][columns];
+        // Initialization of a 1D array
         int[] array = new int[rows*columns];
+        // Adding input to the 2D array
         System.out.print("Enter elements of the 2D array: ");
         for ( int i = 0; i < rows; i++){
             for ( int j = 0; j < columns; j++ ){
                 arr[i][j] = input.nextInt();
             }
         }
+        // Displaying the elements of the 2D array
         System.out.println("The elements of the 2d array is: ");
         for ( int i = 0; i < rows; i++){
             for ( int j = 0; j < columns; j++ ){
@@ -22,6 +29,7 @@ public class Array2D {
             }
             System.out.println();
         }
+        // Converting 2D to 1D array
         int index = 0;
         for ( int i = 0; i <rows; i++){
             for ( int j = 0; j < columns; j++ ){
@@ -29,6 +37,7 @@ public class Array2D {
                 index++;
             }
         }
+        // Displaying elements of the 1D array
         System.out.println("The 1D array is: ");
         for ( int i = 0; i < rows*columns; i++){
             System.out.print(array[i] + " ");
