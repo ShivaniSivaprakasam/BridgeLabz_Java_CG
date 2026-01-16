82d744e5f59931ac24f0aa7c6e4d340962bf6f83
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 16:45:32 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level2/PowerWhile.java
+++ src/CoreProgramming/ControlFlowPractices/Level2/PowerWhile.java
@@ -3,20 +3,24 @@ import java.util.Scanner;
 public class PowerWhile {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        // Getting user input
         System.out.print("Enter a number: ");
         int number = scan.nextInt();
         System.out.print("Enter a power: ");
         int power = scan.nextInt();
+        // Setting condition to get positive number
         while (number < 0) {
             System.out.print("Enter a positive number: ");
             number = scan.nextInt();
         }
         int result = 1;
         int counter = 0;
+        // Condition to get power of a number
         while ( counter < power ){
             result = number * result;
             counter++;
         }
+        // Displaying results
         System.out.println(result);
     }
 }
