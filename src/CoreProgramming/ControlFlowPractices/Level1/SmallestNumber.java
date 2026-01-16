3c794ede6fc1733c44de3716afe775d3f2208f7d
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 15:53:05 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level1/SmallestNumber.java
+++ src/CoreProgramming/ControlFlowPractices/Level1/SmallestNumber.java
@@ -1,15 +1,16 @@
 package CoreProgramming.ControlFlowPractices.Level1;
-
 import  java.util.Scanner;
 public class SmallestNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // Getting user input
         System.out.print("Enter first number: ");
         int number1 = sc.nextInt();
         System.out.print("Enter second number: ");
         int number2 = sc.nextInt();
         System.out.print("Enter third number: ");
         int number3 = sc.nextInt();
+        // Setting loop to find the smallest number
         if ( number1 < number2 && number1 < number3){
             System.out.println("Is the first number " + number1 + " the smallest? Yes");
         }
