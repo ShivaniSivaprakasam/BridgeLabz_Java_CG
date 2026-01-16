97d462eb58648b283f5e3c9184ba1da59a6abf59
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 16:30:51 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level2/FizzBuzzFor.java
+++ src/CoreProgramming/ControlFlowPractices/Level2/FizzBuzzFor.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class FizzBuzzFor {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Getting user input
         System.out.print("Enter a number: ");
         int number =  input.nextInt();
         int i = 1;
+        // Condition to check if the number is greater than 1
         while (i <= number) {
+            // Condition to check divisibility by 3 and 5
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
             }
@@ -19,6 +22,7 @@ public class FizzBuzzFor {
             else {
                 System.out.println(i);
             }
+            // Incrementing i
             i++;
         }
     }
