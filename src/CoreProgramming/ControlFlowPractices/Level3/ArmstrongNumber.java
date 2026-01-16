1fd5f517b5d3f0404e179161c6dc5a0873d9dd20
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 16:52:35 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level3/ArmstrongNumber.java
+++ src/CoreProgramming/ControlFlowPractices/Level3/ArmstrongNumber.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class ArmstrongNumber {
     public static void main(String[] args){
        Scanner input = new Scanner(System.in);
+        // Getting user input
        System.out.print("Enter number: ");
       int N = input.nextInt();
        int length = (int) Math.log10(N) + 1;
@@ -10,12 +11,14 @@ public class ArmstrongNumber {
        int original = N;
       System.out.println("The original number is: " +original);
        int sum = 0;
+        // Setting condition to get values more than zero
         while ( N > 0){
            int digit = N%10;
           sum += Math.pow(digit,length);
            N = N/10;
        }
         System.out.println("The sum of the given number is: " +sum);
+        // Checking the condition
         if ( original == sum ){
            System.out.println("The given number is a armstrong number.");
        }
