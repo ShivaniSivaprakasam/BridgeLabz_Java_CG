24c1215ad4cec38608dbdef5942891b0d2c3f2d4
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 15:39:38 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level1/Integers.java
+++ src/CoreProgramming/ControlFlowPractices/Level1/Integers.java
@@ -1,11 +1,12 @@
 package CoreProgramming.ControlFlowPractices.Level1;
-
 import java.util.Scanner;
 public class Integers {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Getting user input
         System.out.print("Enter a number: ");
         int number = input.nextInt();
+        // Initialization of loop to determine the sign of the number
         if ( number > 0 ){
             System.out.println("Positive.");
         }
