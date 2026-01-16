6f828fae473bf01a5c80933d756db3d098bfdb17
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 15:58:59 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level1/SumOfNumbers.java
+++ src/CoreProgramming/ControlFlowPractices/Level1/SumOfNumbers.java
@@ -1,16 +1,18 @@
 package CoreProgramming.ControlFlowPractices.Level1;
-
 import java.util.Scanner;
 public class SumOfNumbers{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        // Getting user input
         System.out.print("Enter number: ");
         int number = input.nextInt();
+        // Condition to get only natural number
         if ( number <= 0){
             System.out.println("The number " +number +" is not a natural number.");
             return;
         }
         int sum = 0;
+        // Getting sum of natural number
         for ( int i = 1; i <= number; i++){
             sum = number * (number + 1) / 2;
         }
