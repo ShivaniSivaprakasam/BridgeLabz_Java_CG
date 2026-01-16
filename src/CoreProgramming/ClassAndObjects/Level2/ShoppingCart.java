22c1f672a223a8f249723875989df115f20b6d50
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 15:18:03 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ClassAndObjects/Level2/ShoppingCart.java
+++ src/CoreProgramming/ClassAndObjects/Level2/ShoppingCart.java
@@ -1,5 +1,6 @@
 package CoreProgramming.ClassAndObjects.Level2;
 import java.util.Scanner;
+// Created a class cart item
 class CartItem {
     private String itemName;
     private double price;
@@ -21,12 +22,13 @@ class CartItem {
         System.out.println("Total Cost: ₹" + totalCost);
     }
 }
-
+// Created a main class shopping cart
 public class ShoppingCart{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Created an object for cart
         CartItem cart = new CartItem();
-
+        // Getting user input
         System.out.print("Enter item name: ");
         String name = input.nextLine();
 
@@ -42,7 +44,7 @@ public class ShoppingCart{
         System.out.print("Do you want to remove the item? (yes/no): ");
         input.nextLine();
         String choice = input.nextLine();
-
+        // Calling method
         if (choice.equalsIgnoreCase("yes")) {
             cart.removeItem();
             cart.displayTotalCost();
