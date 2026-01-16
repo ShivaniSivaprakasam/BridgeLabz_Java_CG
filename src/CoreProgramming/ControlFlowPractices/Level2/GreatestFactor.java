2aa393a757eec168c9dae809fe86f2c868261c12
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 16:31:59 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level2/GreatestFactor.java
+++ src/CoreProgramming/ControlFlowPractices/Level2/GreatestFactor.java
@@ -3,16 +3,18 @@ import  java.util.Scanner;
 public class GreatestFactor {
         public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
+                // Getting uer input
             System.out.print("Enter a number: ");
             int number = input.nextInt();
             int greatestFactor = 1;
-
+                // Loop set to find the greatest factor
             for (int i = number - 1; i >= 1; i--) {
                 if (number % i == 0) {
                     greatestFactor = i;
                     break;
                 }
             }
+                // Displaying result
             System.out.println("Greatest factor: " + greatestFactor);
         }
 }
