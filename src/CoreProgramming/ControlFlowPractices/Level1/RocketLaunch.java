dd0784b85a2a88b7055624f42b57d377d85c0e1c
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 15:49:47 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level1/RocketLaunch.java
+++ src/CoreProgramming/ControlFlowPractices/Level1/RocketLaunch.java
@@ -1,15 +1,17 @@
 package CoreProgramming.ControlFlowPractices.Level1;
-
 import java.util.Scanner;
 public class RocketLaunch {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Getting user input
         System.out.print("Enter number: ");
         int number = input.nextInt();
+        // Setting while loop to get values greater than 1
         while ( number < 1){
             System.out.print("Enter a number greater than 1: ");
             number = input.nextInt();
         }
+        // Initialization of for loop to start count down
         for ( int i = number; i >= 1; i-- ){
             System.out.println("Countdown: " +i);
         }
