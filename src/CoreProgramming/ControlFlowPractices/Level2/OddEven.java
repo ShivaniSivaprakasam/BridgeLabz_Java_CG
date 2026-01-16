b474d906c5ab53a039e67785659860a95ea2e97e
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 16:41:47 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level2/OddEven.java
+++ src/CoreProgramming/ControlFlowPractices/Level2/OddEven.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 public class OddEven {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Getting user input
         System.out.print("Enter a number: ");
         int number = input.nextInt();
+        // Setting conditions to get only positive non-zero number
         while (number <= 0){
             System.out.print("Enter a positive non-zero number: ");
             number = input.nextInt();
         }
         int countEven = 0;
         int countOdd = 0;
+        // Setting loop to count the number of even and odd number
         for (int i = 1; i <= number; i++){
             if ( i % 2 == 0 ){
                 System.out.println(i + " Even");
@@ -21,6 +24,7 @@ public class OddEven {
                 countOdd++;
             }
         }
+        // Displaying results
         System.out.println("The count of even numbers found in number from 1 to " +number +" are: " + countEven);
         System.out.println("The count of odd numbers found in number from 1 to " +number +" are: " + countOdd);
     }
