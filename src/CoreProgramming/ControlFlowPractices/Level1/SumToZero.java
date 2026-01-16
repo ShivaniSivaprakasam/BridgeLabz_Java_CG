49750447524473094e5b10dcd8d8c7fdca99df50
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 16:24:48 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level1/SumToZero.java
+++ src/CoreProgramming/ControlFlowPractices/Level1/SumToZero.java
@@ -1,18 +1,20 @@
 package CoreProgramming.ControlFlowPractices.Level1;
-
 import java.util.Scanner;
 public class SumToZero {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Getting user input
         System.out.print("Enter number: ");
         double value = input.nextDouble();
         double total = 0.0;
+        // Setting the loop to get values greater than zero
         while ( value > 0){
             total = total + value;
             System.out.println("The sum is: " +total);
             System.out.print("Enter number: ");
             value = input.nextDouble();
         }
+        // Displaying results
         System.out.println("The sum achieved before reaching zeros is: " +total);
         System.out.println("The user has entered zero");
     }
