c9c9917b189e76218e5b73b9e5c470e49996a2d8
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 15:28:54 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level1/EligibilityCheck.java
+++ src/CoreProgramming/ControlFlowPractices/Level1/EligibilityCheck.java
@@ -1,15 +1,17 @@
 package CoreProgramming.ControlFlowPractices.Level1;
-
 import java.util.Scanner;
 public class EligibilityCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Getting user input
         System.out.print("Enter age: ");
         int age = input.nextInt();
+        // While loop to prevent user from entering invalid age
         while ( age <= 0 ){
             System.out.print("Please enter a valid age: ");
             age = input.nextInt();
         }
+        // Setting if loop to check voting age
         if ( age >= 18 ){
             System.out.print("The person\'s age is " +age +" and can vote.");
         }
