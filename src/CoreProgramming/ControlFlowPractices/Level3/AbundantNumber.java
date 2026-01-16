e96d4d7ebd2a28e4073c67f1e90084bececb48f3
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 16:50:13 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level3/AbundantNumber.java
+++ src/CoreProgramming/ControlFlowPractices/Level3/AbundantNumber.java
@@ -3,20 +3,24 @@ import java.util.Scanner;
 public class AbundantNumber {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        // Getting user input
         System.out.print("Enter a number: ");
         int N = input.nextInt();
         int original = N;
+        // Setting condition to get a positive number
         while (N < 0) {
             System.out.print("Enter a positive integer: ");
             N = input.nextInt();
         }
         int sum = 0;
+        // Setting loop to get sum 
         for (int i = 1; i < N; i++) {
             if (N % i == 0) {
                 sum += i;
             }
         }
         System.out.println("Sum of divisors: " + sum);
+        // Checking the condition
         if (sum > original) {
             System.out.println("The given number is an abundant number");
         } else {
