4fceb408ce546916996a7655c925820d263ab1b6
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 15:37:37 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level1/Factorial.java
+++ src/CoreProgramming/ControlFlowPractices/Level1/Factorial.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Getting user input
         System.out.print("Enter a number: ");
         int number = input.nextInt();
+        // Setting while loop to get positive non-zero number
         while (number <= 0){
             System.out.print("Enter a positive non-zero number: ");
             number = input.nextInt();
         }
         int factorial = 1;
         int i = 1;
+        // Finding factorial
         while (i <= number) {
             factorial = factorial * i;
             i++;
