ab920b660d7c2bcce426cc682975520dd68d44fa
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 16:23:51 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level1/SumToNegative.java
+++ src/CoreProgramming/ControlFlowPractices/Level1/SumToNegative.java
@@ -1,10 +1,11 @@
 package CoreProgramming.ControlFlowPractices.Level1;
-
 import java.util.Scanner;
 public class SumToNegative {
     public static void main(String[] args) {
+        // Getting user inpu
         Scanner input = new Scanner(System.in);
         double total = 0.0;
+        // Setting condition to get only natural number
         while (true) {
             System.out.print("Enter number: ");
             double value = input.nextDouble();
@@ -14,6 +15,7 @@ public class SumToNegative {
             total = total + value;
             System.out.println("The sum is: " + total);
         }
+        // Displaying results
         System.out.println("The sum achieved before reaching zero or a negative number is: " + total);
         System.out.println("The user has entered zero or a negative number");
     }
