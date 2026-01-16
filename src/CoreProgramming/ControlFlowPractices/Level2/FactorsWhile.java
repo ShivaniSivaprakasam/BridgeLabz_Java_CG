649ee1bffd4a8f8aa14cb3b2b8f13d95fd93f0a0
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 16:27:52 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level2/FactorsWhile.java
+++ src/CoreProgramming/ControlFlowPractices/Level2/FactorsWhile.java
@@ -1,12 +1,15 @@
 package CoreProgramming.ControlFlowPractices.Level2;
 import java.util.Scanner;
 public class FactorsWhile {
+    // Created method to find factors
     public static void findingFactors(int N){
+        // Setting loop condition to only positive numbers
         while ( N < 0 ) {
             System.out.print("Enter a positive integer: ");
             N = new Scanner(System.in).nextInt();
         }
         int count = 0;
+        // Setting conditions to count number
         while ( count < N ){
             count++;
             if ( N % count == 0 ){
@@ -16,8 +19,10 @@ public class FactorsWhile {
     }
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        // Getting user input
         System.out.print("Enter a number: ");
         int N = input.nextInt();
+        // Calling the method
         findingFactors(N);
     }
 }
