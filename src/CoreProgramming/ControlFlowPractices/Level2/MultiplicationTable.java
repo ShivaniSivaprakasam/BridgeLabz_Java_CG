9d969010a5c9c9fa77ee40d7aeaa97825e3e3a62
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 16:40:07 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level2/MultiplicationTable.java
+++ src/CoreProgramming/ControlFlowPractices/Level2/MultiplicationTable.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class MultiplicationTable {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Gettting user input
         System.out.print("Enter a number: ");
         int number = input.nextInt();
         int table = 0;
+        // Setting loop conditions to get multipes from 6 to 9
         for ( int i = 6; i <= 9; i++ ){
             table = number * i;
             System.out.println("The multiplication table for the number " +number +" from 6 to 9 is: " +table);
