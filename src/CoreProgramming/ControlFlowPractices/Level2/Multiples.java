38a9f96d730aba51e750bb19d7f44f92892260eb
ShivaniSivaprakasam <shivaniavanikkarasi@gmail.com>
2026-01-16 16:38:00 +0530

    [Shivani] : Added : comment lines to the code

--- src/CoreProgramming/ControlFlowPractices/Level2/Multiples.java
+++ src/CoreProgramming/ControlFlowPractices/Level2/Multiples.java
@@ -3,12 +3,15 @@ import  java.util.Scanner;
 public class Multiples {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Getting user input
         System.out.print("Enter a number: ");
         int number = input.nextInt();
+        // Setting while loop to get only number less than 100
         while ( number > 100 ){
             System.out.print("Enter a number less than 100: ");
             number = input.nextInt();
         }
+        // Checking divisibility
         for ( int i = 1; i <= 100; i++){
             if ( number % i == 0){
                 System.out.println(i);
